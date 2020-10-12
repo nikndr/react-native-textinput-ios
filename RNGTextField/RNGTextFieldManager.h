a5632e77827dbc11739778058525e2f1c12b3229
nikandr.marhal <nikandr.marhal@fortegrp.com>
2020-10-12 16:27:29 +0300

    moved view manager into separate Obj-C class

---
+++ RNGTextField/RNGTextFieldManager.h
@@ -0,0 +1,18 @@
+//
+//  RNGTextFieldManager.h
+//  RNGTextField
+//
+//  Created by Nikandr Marhal on 12.10.2020.
+//  Copyright © 2020 Eafy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+#import <React/RCTViewManager.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RNTextFieldManager : RCTViewManager<UITextFieldDelegate>
+
+@end
+
+NS_ASSUME_NONNULL_END
--- RNGTextField/RNGTextFieldManager.m
+++ RNGTextField/RNGTextFieldManager.m
@@ -1,20 +1,15 @@
 //
-//  RNGTextFieldManager.m
-//  taochemao
+//  RNTextField.m
+//  RevenueWell
 //
-//  Created by Eafy on 2018/7/31.
-//  Copyright © 2018年 emao. All rights reserved.
+//  Created by Nikandr Marhal on 12.10.2020.
 //
 
 #import <Foundation/Foundation.h>
-#import <React/RCTViewManager.h>
-#import "RNGTextField.h"
 #import <React/RCTFont.h>
+#import "RNTextField.h"
 
-@interface RNGTextFieldManager : RCTViewManager<UITextFieldDelegate>
-@end
-
-@implementation RNGTextFieldManager
+@implementation RNTextFieldManager
 
 RCT_EXPORT_MODULE()
 
@@ -24,33 +19,29 @@ RCT_EXPORT_VIEW_PROPERTY(text, NSString)
 RCT_EXPORT_VIEW_PROPERTY(onChange, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(onEndEditing, RCTBubblingEventBlock)
 RCT_EXPORT_VIEW_PROPERTY(returnKeyType, UIReturnKeyType)
-RCT_CUSTOM_VIEW_PROPERTY(placeholderTextColor, UIColor, RNGTextField)
+RCT_CUSTOM_VIEW_PROPERTY(placeholderTextColor, UIColor, RNTextField)
 {
     [view setValue: [RCTConvert UIColor:json] forKeyPath: @"_placeholderLabel.textColor"];
 }
-RCT_CUSTOM_VIEW_PROPERTY(textAlign, NSTextAlignment, RNGTextField)
+RCT_CUSTOM_VIEW_PROPERTY(textAlign, NSTextAlignment, RNTextField)
 {
     [view setTextAlignment:[RCTConvert NSTextAlignment:json]];
 }
 RCT_EXPORT_VIEW_PROPERTY(textColor, UIColor)
-RCT_CUSTOM_VIEW_PROPERTY(fontSize, NSNumber, RNGTextField)
+RCT_CUSTOM_VIEW_PROPERTY(fontSize, NSNumber, RNTextField)
 {
     view.font = [RCTFont updateFont:view.font withSize:json ?: @(defaultView.font.pointSize)];
 }
-//RCT_CUSTOM_VIEW_PROPERTY(placeholderTextColor, UIColor, RNGTextField){
-//    view.placeholderTextColor = [RCTConvert UIColor:json];
-//}
 
 - (UIView *)view
 {
-    RNGTextField *textField = [[RNGTextField alloc] init];
+    RNTextField *textField = [[RNTextField alloc] init];
     textField.delegate = self;
     [textField addTarget:self action:@selector(textFieldDidChanged:) forControlEvents:UIControlEventEditingChanged];
     return textField;
 }
 
-
-- (void)textFieldDidChanged:(RNGTextField *)textField {
+- (void)textFieldDidChanged:(RNTextField *)textField {
     UITextRange *range = textField.markedTextRange;
     NSUInteger maxLength = textField.maxLength.unsignedIntegerValue;
 
@@ -66,7 +57,7 @@ RCT_CUSTOM_VIEW_PROPERTY(fontSize, NSNumber, RNGTextField)
     }
 }
 
-- (void)textFieldDidEndEditing:(RNGTextField *)textField {
+- (void)textFieldDidEndEditing:(RNTextField *)textField {
     if (textField.onEndEditing) {
         textField.onEndEditing(@{@"text": textField.attributedText.string});
     }
